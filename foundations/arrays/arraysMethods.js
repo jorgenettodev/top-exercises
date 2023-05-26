8493c1ca62500ba76380b2bdeaecf063cbcbf2bf
Jorge Netto <jorgenetto96@gmail.com>
2023-05-26 04:49:12 -0300

    Methods: find, findIndex, reduce, map(), filter().

--- foundations/arrays/arraysMethods.js
+++ foundations/arrays/arraysMethods.js
@@ -8,16 +8,38 @@ const items = [
     {name: 'Keyboard', price: 25}
 ]
 
-// get all items that the price are equal or lower than 100 dolars.
+// get all items that the price are equal or lower than 100 dollars.
 
 let itemsBelow100 = items.filter(item => item.price <= 100);
 
-console.table(itemsBelow100);
+// console.table(itemsBelow100);
 
-// get an array with only the name of items
+// get an array with only the name of items using map()
 
 let itemNames = items.map((item) => {
     return item.name;
 })
 
-console.table(itemNames);
+// console.table(itemNames);
+
+// Use Array.find() to find an item, for instance 'Bike'.
+
+const foundBookIndex = items.findIndex(item => item.name === 'Book');
+
+ console.log(items[foundBookIndex]);
+
+// get the sum of all prices combined. 
+
+let totalPrice = items.reduce((total, item) => {
+    return total + item.price;
+} ,0)
+
+// console.log(totalPrice);
+
+// remove the item 'Book'
+items.splice(foundBookIndex, 1);
+console.table(items);
+
+items.forEach((item) => {
+    console.log(item.name + ',' + item.price);
+})
